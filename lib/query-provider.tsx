43087ab5ecf37d34551dba6142a39c6eb91aedc8
protocyber <fitrah.pro@gmail.com>
2025-11-11 23:52:24 +0700

    feat: enable react-query-devtools

--- lib/query-provider.tsx
+++ lib/query-provider.tsx
@@ -3,11 +3,13 @@
 import { ReactNode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./query-client";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function QueryProvider({ children }: { children: ReactNode; }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
     </QueryClientProvider>
   );
 }
